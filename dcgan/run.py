bbf44f868985b7616642fd1514e6f6febc72cbe4
dingnigefei <zluo6@illinois.edu>
2017-03-27 17:27:07 -0500

    dcgan cleanup

--- dcgan/run.py
+++ dcgan/run.py
@@ -13,18 +13,18 @@ from torch.autograd import Variable
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset_path', type=str, default='/home/alan/datable/cifar10', help='path to dataset')
-parser.add_argument('--num_workers', type=int, default=20, help='number of data loading workers')
-parser.add_argument('--batch_size', type=int, default=64, help='input batch size')
-parser.add_argument('--image_size', type=int, default=64, help='the height / width of the input image to network')
+parser.add_argument('--dataset_path', type=str, default='/home/alan/datable/cifar10')
+parser.add_argument('--num_workers', type=int, default=20)
+parser.add_argument('--batch_size', type=int, default=64)
+parser.add_argument('--image_size', type=int, default=64)
 parser.add_argument('--in_channels', type=int, default=3)
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
-parser.add_argument('--num_epochs', type=int, default=100, help='number of epochs to train for')
-parser.add_argument('--lr', type=float, default=2e-4, help='learning rate, default=0.0002')
-parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
-parser.add_argument('--num_gpus', type=int, default=2, help='number of GPUs to use')
+parser.add_argument('--num_epochs', type=int, default=100)
+parser.add_argument('--lr', type=float, default=2e-4)
+parser.add_argument('--beta1', type=float, default=0.5)
+parser.add_argument('--num_gpus', type=int, default=2)
 parser.add_argument('--load_ckpt', type=bool, default=False)
 parser.add_argument('--ckpt_path', type=str, default='/home/alan/datable/cifar10/ckpt')
 parser.add_argument('--print_every', type=int, default=50)
@@ -69,8 +69,9 @@ for epoch in range(opt.num_epochs):
     noise = Variable(torch.zeros(batch_size, opt.nz, 1, 1).normal_(0, 1).type(torch.cuda.FloatTensor))
 
     ''' (1) Update D network: maximize log(D(x)) + log(1 - D(G(z))) '''
-    # train with real
     net_d.zero_grad()
+
+    # train with real
     output = net_d(images_real)
     loss_d_real = criterion(output, labels_real[:batch_size])
     loss_d_real.backward()
@@ -81,16 +82,19 @@ for epoch in range(opt.num_epochs):
     output = net_d(images_fake.detach())
     loss_d_fake = criterion(output, labels_fake[:batch_size])
     loss_d_fake.backward()
-    D_G_z1 = output.data.mean()
+    D_g_z1 = output.data.mean()
+
     loss_d = loss_d_real+loss_d_fake
     optimizer_d.step()
 
     ''' (2) Update G network: maximize log(D(G(z))) '''
     net_g.zero_grad()
-    output = net_d(fake)
+
+    output = net_d(images_fake)
     loss_g = criterion(output, labels_real[:batch_size])
     loss_g.backward()
-    D_G_z2 = output.data.mean()
+    D_g_z2 = output.data.mean()
+
     optimizer_g.step()
 
     losses_g.update(loss_g.data[0], batch_size)
@@ -100,9 +104,9 @@ for epoch in range(opt.num_epochs):
       logger.info('epoch {}/{}, step {}/{}: '
                   'loss_g={loss_g.val:.4f}, avg loss_g={loss_g.avg:.4f}, '
                   'loss_d={loss_d.val:.4f}, avg loss_d={loss_d.avg:.4f}, '
-                  'D(x): {D_x:.4f}, D(G(z)): {D_G_z1:.4f}/{D_G_z2:.4f}'
+                  'D(x): {D_x:.4f}, D(G(z)): {D_g_z1:.4f}/{D_g_z2:.4f}'
                   .format(epoch, opt.num_epochs, step, len(data_loader), loss_g=losses_g, loss_d=losses_d,
-                          D_x=D_x, D_G_z1=D_G_z1, D_G_z2=D_G_z2))
+                          D_x=D_x, D_g_z1=D_g_z1, D_g_z2=D_g_z2))
 
     if step == 0:
       torchvision.utils.save_image(images, '%s/real_samples.png'%opt.ckpt_path)
