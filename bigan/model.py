6f1dd23446a540f333610a7f2e0cd9a8d6a432b9
Yuliang-Zou <zouyuliang123@gmail.com>
2017-03-28 11:43:56 -0700

    Add dropout layers in Disc, change last layer in P to Tanh.

--- bigan/model.py
+++ bigan/model.py
@@ -30,7 +30,7 @@ class P(nn.Module):
       nn.LeakyReLU(0.01, inplace=True),
       # state dim: 32 x 32 x 32
       nn.Conv2d(32, opt.in_channels, 1, stride=1, bias=False),
-      nn.Sigmoid()
+      nn.Tanh()
       # output dim: in_channels x 32 x 32
     )
 
@@ -106,21 +106,26 @@ class Discriminator(nn.Module):
     self.inference_x = nn.Sequential(
       # input dim: in_channels x 32 x 32 (no bn)
       nn.Conv2d(3, 32, 5, stride=1, bias=False),
+      nn.Dropout2d(p=0.2),
       nn.LeakyReLU(0.01, inplace=True),
       # state dim: 32 x 28 x 28
       nn.Conv2d(32, 64, 4, stride=2, bias=False),
+      nn.Dropout2d(p=0.2),
       nn.BatchNorm2d(64),
       nn.LeakyReLU(0.01, inplace=True),
       # state dim: 64 x 13 x 13
       nn.Conv2d(64, 128, 4, stride=1, bias=False),
+      nn.Dropout2d(p=0.2),
       nn.BatchNorm2d(128),
       nn.LeakyReLU(0.01, inplace=True),
       # state dim: 128 x 10 x 10
       nn.Conv2d(128, 256, 4, stride=2, bias=False),
+      nn.Dropout2d(p=0.2),
       nn.BatchNorm2d(256),
       nn.LeakyReLU(0.01, inplace=True),
       # state dim: 256 x 4 x 4
       nn.Conv2d(256, 512, 4, stride=1, bias=False),
+      nn.Dropout2d(p=0.2),
       nn.BatchNorm2d(512),
       nn.LeakyReLU(0.01, inplace=True)
       # state dim: 512 x 1 x 1
@@ -129,9 +134,11 @@ class Discriminator(nn.Module):
     self.inference_z = nn.Sequential(
       # input dim: z_dim x 1 x 1 (no bn)
       nn.Conv2d(opt.z_dim, 512, 1, stride=1, bias=False),
+      nn.Dropout2d(p=0.2),
       nn.LeakyReLU(0.01, inplace=True),
       # state dim: 512 x 1 x 1 (no bn)
       nn.Conv2d(512, 512, 1, stride=1, bias=False),
+      nn.Dropout2d(p=0.2),
       nn.LeakyReLU(0.01, inplace=True)
       # output dim: 512 x 1 x 1
     )
@@ -139,12 +146,15 @@ class Discriminator(nn.Module):
     self.inference_joint = nn.Sequential(
       # input dim: 1024 x 1 x 1 (no bn)
       nn.Conv2d(1024, 1024, 1, stride=1, bias=False),
+      nn.Dropout2d(p=0.2),
       nn.LeakyReLU(0.01, inplace=True),
       # state dim: 1024 x 1 x 1 (no bn)
       nn.Conv2d(1024, 1024, 1, stride=1, bias=False),
+      nn.Dropout2d(p=0.2),
       nn.LeakyReLU(0.01, inplace=True),
       # state dim: 1024 x 1 x 1 (no bn)
       nn.Conv2d(1024, 1, 1, stride=1, bias=False),
+      nn.Dropout2d(p=0.2),
       # output dim: 1 x 1 x 1
       nn.Sigmoid()
     )
